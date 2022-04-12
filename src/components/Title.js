5bdbdb652c76772a8e55db29bbc7cac96ac3a557
thiet <thiet@classfunc.com>
2022-04-12 18:27:37 +0700

    useCallback

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,17 +1,16 @@
 import React, {memo} from 'react';
+import {Button} from "antd";
 
-function Title({count}) {
+function Title({handleChangeCount}) {
     console.log("render title")
     return (
-        <h2 className={"text-center text-[24px]"}>To do list </h2>
+        <>
+            <h2 className={"text-center text-[24px]"}>To do list </h2>
+            <Button onClick={handleChangeCount} type="primary">Click</Button>
+
+        </>
     );
 }
 
-function areEqual(prevProps, nextProps) {
-    /*
-      trả về true khi prevProps = nextProps // ko cần re-render
-      ngược lại trả về false
-    */
-}
 
 export default memo(Title);
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import 'antd/dist/antd.min.css'
 import Item from "./Item";
 import AddItem from "./AddItem";
@@ -7,15 +7,15 @@ import Title from "./Title";
 function TodoList(props) {
     const [listItems, setListItems] = useState([])
     const [count, setCount] = useState(0)
-    const handleChangeCount = () => {
-        setCount(count + 1)
-    }
+    const handleChangeCount = useCallback(() => {
+        setCount(prevState => prevState + 1)
+    }, [])
 
     return (
         <div className={"w-[500px] bg-[#fff] mx-auto rounded-md p-5"}>
-            <Title count={count}/>
-            {/*<Button onClick={handleChangeCount} type="primary">Click</Button>*/}
-            {/*{count}*/}
+            <Title handleChangeCount={handleChangeCount}/>
+
+            {count}
             <AddItem setListItems={setListItems}/>
             {
                 listItems?.length ? listItems.map((item, index) =>
